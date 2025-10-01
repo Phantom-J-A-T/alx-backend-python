4f7ae03aa8f4069c3e2f4d30fcff92a7e3400a31
Phantom <ramonadedotun550@gmail.com>
2025-10-01 23:14:23 +0100

    Creating the directory for the queries and adding the query file

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import sqlite3
+import functools
+
+#### decorator to log SQL queries
+def log_queries(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        # Try to get query from kwargs or positional args
+        query = kwargs.get("query") if "query" in kwargs else (args[0] if args else None)
+        if query:
+            print(f"[LOG] Executing SQL Query: {query}")
+        else:
+            print("[LOG] No SQL query found.")
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+
+#### fetch users while logging the query
+if __name__ == "__main__":
+    users = fetch_all_users(query="SELECT * FROM users")
+    print(users)
